5ace73e30b6bb62e3bf0c00d21ed428732d02a7d
user03579 <132360471+user03579@users.noreply.github.com>
2023-12-02 15:45:06 +0900

    Add files via upload

---
+++ motor_io.c
@@ -0,0 +1,38 @@
+﻿#include "motor_io.h"
+
+// DC 모터 드라이버가 연결된 포트 PB6과 PB7을 출력으로 설정, 모두 0 출력
+void mtr_io_init(void)
+{
+	DDRB |= 0xC0;	// 1100_0000
+	PORTB &= 0x3F;	// 0011_1111
+}
+
+// DC 모터 구동
+void mtr_io_move(int iDir)
+{
+	switch(iDir) {
+		case MOTOR_MOVE_CW:	PORTB = (PORTB & 0x3F) | MOTOR_CW; break;
+		case MOTOR_MOVE_CCW:	PORTB = (PORTB & 0x3F) | MOTOR_CCW; break;
+		default: PORTB = (PORTB & 0x3F) | MOTOR_STOP;
+	}
+}
+
+// PWM파의 듀티비 제어 --> DC 모터 구동 속도 제어
+// PWM파의 주기: 100[msec]
+// uSpeed: (0 - 100) --> (듀티비: 0% - 100%)
+void mtr_io_dir_spd(int iDir, uint uSpeed, float fSec)
+{	
+	int iTimes;
+
+	if(uSpeed > 100) uSpeed = 100;	// 100으로 제한
+	if(fSec < 0.1) fSec = 0.1;
+	iTimes = fSec/0.1;	// for 루프 실행 횟수 = fSec[sec] / 100[msec]
+
+	for(int i=0; i<iTimes; i++) {	// 루프 1회 실행 --> 약 100[msec]
+		mtr_io_move(iDir);					// 모터 구동
+		_delay_ms(uSpeed);	
+		mtr_io_move(MOTOR_MOVE_STOP);	// 모터 정지
+		_delay_ms(100 - uSpeed);	
+	}
+}
+ 
---
+++ motor_io.h
@@ -0,0 +1,20 @@
+﻿#ifndef MOTOR_IO_H_
+#define MOTOR_IO_H_
+
+#include "main.h"
+
+// 포트 PB7과 PB6에 출력할 값
+#define MOTOR_CW	(0x2 << 6)	// "10", 시계 방향(clockwise)
+#define MOTOR_CCW	(0x1 << 6)	// "01", 반시계 방향(counterclockwise)
+#define MOTOR_STOP	(0x0 << 6)	// "00", 정지
+
+// mtr_io_move() 함수에 전달될 값
+#define MOTOR_MOVE_STOP	0	// 정지
+#define MOTOR_MOVE_CW	1	// 시계 방향(clockwise) 구동
+#define MOTOR_MOVE_CCW	2	// 반시계 방향(counterclockwise) 구동
+
+void mtr_io_init(void);
+void mtr_io_move(int iDir);
+void mtr_io_dir_spd(int iDir, uint uSpeed, float fSec);
+
+#endif /* MOTOR_IO_H_ */ 
